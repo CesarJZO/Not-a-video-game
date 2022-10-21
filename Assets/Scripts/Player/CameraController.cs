63274d5d98266a2d97e8c440ff63c4ba46ee79a5
CesarJZO <cesarjzo@outlook.com>
2022-10-21 15:34:00 -0500

    Add camera functionality

--- Assets/Scripts/Player/CameraController.cs
+++ Assets/Scripts/Player/CameraController.cs
@@ -4,6 +4,20 @@ namespace Player
 {
     public class CameraController : MonoBehaviour
     {
-    
+        [SerializeField] private Transform target;
+        [SerializeField] private Transform player;
+        [SerializeField] private float smoothTime;
+        
+        private Vector3 _velocity;
+        
+        private void Update()
+        {
+            transform.position = Vector3.SmoothDamp(transform.position, target.position, ref _velocity, smoothTime);
+        }
+
+        private void LateUpdate()
+        {
+            transform.LookAt(player);
+        }
     }
 }
