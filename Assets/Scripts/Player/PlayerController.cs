4960c8e4ba31d367b66ed2d71f4b17b71c7f6cf2
Smith hernadez <smitherdezzhotmail.com>
2022-10-21 17:01:23 -0500

    duplicate scene

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -1,9 +1,37 @@
 ﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
 
-namespace Player
+public class CamaraJugado : MonoBehaviour
 {
-    public class PlayerController : MonoBehaviour
-    {
+    public float sensX;
+    public float sensY;
+    public Transform orientacion;
+
+    float xRotacion;
+    float yRotacion;
     
+    private void Start(){
+        Cursor.lockState= CursorLockMode.Locked;
+        Cursor.visible= false;
     }
+
+    private void Update(){
+        float mouseX=Input.GetAxis("Mouse X")*Time.deltaTime *sensX;
+        float mouseY=Input.GetAxis("Mouse Y")*Time.deltaTime *sensY;
+
+        yRotacion += mouseX;
+        xRotacion -= mouseY;
+        xRotacion = Mathf.Clamp(xRotacion, -96f,-96f);
+
+        //rotacion de la camara y orientacion 
+
+        transform.rotation= Quaternion.Euler(xRotacion, yRotacion, 0);
+        orientacion.rotation =Quaternion.Euler(0, yRotacion, 0);    
+         
+    }
+
+
 }
+
+
