e1b1bc1558bf17523042e1342cda8d98cfa41597
Smith hernadez <smitherdezzhotmail.com>
2022-10-22 23:23:39 -0500

    a little change

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -6,13 +6,21 @@ using System.Diagnostics;
     public class PlayerController : MonoBehaviour{
 
         float speed =5f;
+        public float rotationSped =50f;
         void Update(){
 
         float horizontalInput = Input.GetAxis("Horizontal");
         float verticalInput = Input.GetAxis("Vertical");
         
         Vector3 movementDirection = new Vector3(horizontalInput,0,verticalInput);
+        movementDirection.Normalize();
         transform.position = transform.position + movementDirection * speed *Time.deltaTime;
+    if (movementDirection!= Vector3.zero){
+        transform.rotation = Quaternion.Slerp(transform.rotation,
+        Quaternion.LookRotation(movementDirection),
+        rotationSped * Time.deltaTime);
+
+            }
         }
         
     } 
