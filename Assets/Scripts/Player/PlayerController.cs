edfaad5c52cef0ebf45d291e7342bbcecc663b9b
Smith hernadez <smitherdezzhotmail.com>
2022-10-24 19:38:41 -0500

    Listo ya quedo hecho lo de poder saltar solo que me falta que le puedas poner la camara al jugador y sigo sin entender como hacer que puedan coliconar  o subir las escaleras

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -4,9 +4,18 @@ using System.Collections.Generic;
 using System.Diagnostics;
 
     public class PlayerController : MonoBehaviour{
-
-        float speed =5f;
+        Rigidbody rb;
+        float speed =2f;
         public float rotationSped =50f;
+        bool floorDetected = false;
+        bool isJump = false;
+        public float jumpForce = 5.0f;
+        
+        void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+        
         void Update(){
 
         float horizontalInput = Input.GetAxis("Horizontal");
@@ -15,12 +24,32 @@ using System.Diagnostics;
         Vector3 movementDirection = new Vector3(horizontalInput,0,verticalInput);
         movementDirection.Normalize();
         transform.position = transform.position + movementDirection * speed *Time.deltaTime;
-    if (movementDirection!= Vector3.zero){
+        if (movementDirection!= Vector3.zero){
         transform.rotation = Quaternion.Slerp(transform.rotation,
         Quaternion.LookRotation(movementDirection),
         rotationSped * Time.deltaTime);
 
-            }
+        }
+
+        Vector3 floor = transform.TransformDirection(Vector3.down);  
+
+        if (Physics.Raycast(transform.position, floor, 1.03f))
+        {
+            floorDetected = true;
+            //print("Contacto con el suelo");
+        }
+        else
+        {
+            floorDetected = false;
+            //print("No hay contacto con el suelo");
+        }
+
+        isJump = Input.GetButtonDown("Jump");
+
+        if (isJump && floorDetected)
+        {
+            rb.AddForce(new Vector3(0, jumpForce, 0), ForceMode.Impulse);
+        }
         }
         
     } 
