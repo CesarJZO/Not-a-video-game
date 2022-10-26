2add679de3addc50707c4069da8b4d5e80834262
César Zárate <cesarjzo@outlook.com>
2022-10-25 19:03:24 -0500

    Add enemy behavior

--- Assets/Scripts/Enemy/EnemyController.cs
+++ Assets/Scripts/Enemy/EnemyController.cs
@@ -1,34 +1,65 @@
 ﻿using UnityEngine;
+using UnityEngine.AI;
 using StatePattern;
+
 namespace Enemy
 {
     public class EnemyController : MonoBehaviour
     {
+        [Header("Settings")]
+        public LayerMask playerLayer;
+        public float playerSensorRadius;
+        public float idleTime;
+        public float walkTime;
+        public float walkSpeed;
+
+        [Header("Path tracking")]
+        public Transform player;
+
+
         [Header("Dependencies")]
+        public NavMeshAgent navMeshAgent;
         public new Rigidbody rigidbody;
-        
+
+
         #region State Machine
 
         private StateMachine _stateMachine;
         public IdleState idleState;
         public WalkState walkState;
         public ChaseState chaseState;
+        public void ChangeState(State state) => _stateMachine.ChangeState(state);
 
         #endregion
+
+
+        public bool PlayerDetector => Vector3.Distance(transform.position, player.position) < playerSensorRadius;
 
         private void Awake()
         {
             rigidbody = GetComponent<Rigidbody>();
-            
+
             idleState = new IdleState(this);
             walkState = new WalkState(this);
             chaseState = new ChaseState(this);
+
+            _stateMachine = new StateMachine(idleState);
         }
 
         private void Start() => _stateMachine = new StateMachine(idleState);
 
-        private void Update() => _stateMachine.CurrentState.Update();
+        private void Update()
+        {
+            _stateMachine.CurrentState.Update();
+            Debug.Log(_stateMachine.CurrentState);
+        }
 
         private void FixedUpdate() => _stateMachine.CurrentState.FixedUpdate();
+
+        private void OnDrawGizmos()
+        {
+            Gizmos.color = Color.red;
+            Gizmos.DrawWireSphere(transform.position, playerSensorRadius);
+        }
     }
 }
--- Assets/Scripts/Enemy/WalkState.cs
+++ Assets/Scripts/Enemy/WalkState.cs
@@ -1,9 +1,34 @@
-﻿namespace Enemy
+﻿using UnityEngine;
+namespace Enemy
 {
     public class WalkState : EnemyState
     {
+        private Vector3 direction;
         public WalkState(EnemyController enemy) : base(enemy) { }
         
-        
+        public override void Start()
+        {
+            direction = new Vector3
+            {
+                x = Random.Range(-1, 1),
+                z = Random.Range(-1, 1)
+            }.normalized;
+            stateTime = Time.time + enemy.walkTime;
+        }
+
+        public override void Update()
+        {
+            if (Time.time > stateTime)
+                enemy.ChangeState(enemy.idleState);
+        }
+
+        public override void FixedUpdate()
+        {
+            enemy.rigidbody.velocity = direction * enemy.walkSpeed;
+            if (enemy.PlayerDetector)
+                enemy.ChangeState(enemy.chaseState);
+        }
+
+        public override string ToString() => nameof(WalkState);
     }
 }
